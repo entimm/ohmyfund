db9c5b52c455a7f1c1a707c3153bb934a657c8e9
entimm <entimm@gmail.com>
2017-07-31 00:46:23 +0800

    新浪的获取（未启用）

---
+++ app/Console/Commands/UpdateStocksBySina.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\SinaService;
+use App\StockHistories;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class UpdateStocksBySina extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:stocks';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update stocks';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('update stocks 🙏');
+
+        // $this->handleCn();
+        $this->handleUs();
+
+        $this->info('update stocks done 😎');
+    }
+
+    protected function handleCn()
+    {
+        foreach ($this->cnStocks() as $stock) {
+            $touchNum = 0;
+            $stock = $stock[0].$stock[1];
+            $records = resolve(SinaService::class)->resolveCn($stock);
+            DB::transaction(function () use ($records, $stock, &$touchNum) {
+                foreach ($records as $key => $record) {
+                    $record = $this->transformCn($record);
+                    StockHistories::firstOrCreate(['code' => $stock, 'date' => $record['date']], $record);
+                    $touchNum++;
+                }
+            });
+
+            $this->info("{$stock} | {$touchNum}");
+        }
+    }
+
+    private function cnStocks()
+    {
+        return [
+            ['sh', '000001'],
+            ['sh', '000002'],
+            ['sh', '000003'],
+            ['sh', '000016'],
+
+            ['sz', '399001'],
+            ['sz', '399005'],
+            ['sz', '399006'],
+            ['sz', '399300'],
+        ];
+    }
+
+    private function transformCn($record)
+    {
+        $record['date'] = $record['day'];
+        $record['open'] *= 1000;
+        $record['high'] *= 1000;
+        $record['low'] *= 1000;
+        $record['close'] *= 1000;
+        return $record;
+    }
+
+    protected function handleUs()
+    {
+        foreach ($this->usStocks() as $stock) {
+            $touchNum = 0;
+            $records = resolve(SinaService::class)->resolveUs($stock);
+            DB::transaction(function () use ($records, $stock, &$touchNum) {
+                foreach ($records as $key => $record) {
+                    $record = $this->transformUs($record);
+                    StockHistories::firstOrCreate(['code' => $stock, 'date' => $record['date']], $record);
+                    $touchNum++;
+                }
+            });
+
+            $this->info("{$stock} | {$touchNum}");
+        }
+    }
+
+    private function transformUs($record)
+    {
+        $record['date'] = $record['d'];
+        $record['open'] = $record['o'] * 1000;
+        $record['high'] = $record['h'] * 1000;
+        $record['low'] = $record['l'] * 1000;
+        $record['close'] = $record['c'] * 1000;
+        $record['volume'] = $record['v'];
+        return $record;
+    }
+
+    private function usStocks()
+    {
+        return ['ixic', 'dji', 'inx'];
+    }
+}
---
+++ app/Services/SinaService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+
+class SinaService
+{
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    public function resolveCn($stock)
+    {
+        $url = "http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol={$stock}&scale=240&datalen=100000";
+        $content = $this->client->get($url)->getBody()->getContents();
+        $json = $this->makeJson($content);
+        $records = $json ? json_decode($json, true) : [];
+        return $records;
+    }
+
+    public function resolveUs($stock)
+    {
+        $url = "http://stock.finance.sina.com.cn/usstock/api/jsonp_v2.php/var%20_{$stock}=/US_MinKService.getDailyK?symbol=.{$stock}";
+        $content = $this->client->get($url)->getBody()->getContents();
+        $beginPos = strpos($content, '[');
+        $content = substr($content, $beginPos, -2);
+        $json = $this->makeJson($content);
+        $records = $json ? json_decode($json, true) : [];
+        return $records;
+    }
+
+    private function makeJson($content)
+    {
+        if(preg_match('/\w:/', $content)){
+            $json = preg_replace('/(\w+):/is', '"$1":', $content);
+            return $json;
+        }
+        return false;
+    }
+}
