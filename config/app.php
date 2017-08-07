8b92e5f831d07c147a66c9ee3753357765587750
entimm <entimm@gmail.com>
2017-08-07 21:50:21 +0800

    Add l5-repository

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Prettus\Repository\Providers\RepositoryServiceProvider::class,
 
         /*
          * Application Service Providers...
